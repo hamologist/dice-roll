992b12ead509e6f6b8711f7f71385c34760f7aa3
Jordan Paris <hamologist@gmail.com>
2025-01-29 22:55:05 -0500

    Basic hello world fastify setup

--- index.ts
+++ index.ts
@@ -1,1 +1,15 @@
-console.log("Hello via Bun!");
+import Fastify from 'fastify'
+const fastify = Fastify({
+  logger: true
+});
+
+fastify.get('/', async (request, reply) => {
+  return { hello: 'world' }
+});
+
+try {
+  await fastify.listen({ port: 3000 })
+} catch (err) {
+  fastify.log.error(err)
+  process.exit(1)
+}
