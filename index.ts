4661e41e0f326cd5e8d280289d682de091bac226
Jordan Paris <hamologist@gmail.com>
2025-01-29 23:17:59 -0500

    Ports dice roll logic from dice-roll-cdk into project

--- index.ts
+++ index.ts
@@ -1,10 +1,75 @@
-import Fastify from 'fastify'
+import Fastify from 'fastify';
+import { z } from 'zod';
+
+type RollResponse = {
+    step: Array<Step>
+};
+
+type Step = {
+    rolls: Array<Roll>,
+    total: number
+};
+
+type Roll = {
+    count: number,
+    sides: number,
+    modifier: number,
+    rolls: Array<number>,
+    total: number
+};
+
 const fastify = Fastify({
   logger: true
 });
 
-fastify.get('/', async (request, reply) => {
-  return { hello: 'world' }
+const payloadSchema = z.object({
+  count: z.number()
+    .positive()
+    .lt(100),
+  dice: z.object({
+    count: z.number()
+      .positive()
+      .lt(100),
+    sides: z.number()
+      .positive()
+      .lt(1000),
+    modifier: z.number()
+  }).array()
+});
+
+fastify.post('/', async (request) => {
+  const payload = payloadSchema.parse(request.body);
+
+  const rollResponse: RollResponse = {
+    step: []
+  };
+  for (let rollCount = payload.count || 1; rollCount > 0; rollCount--) {
+      const step: Step = {
+          rolls: [],
+          total: 0
+      };
+      for (const dice of payload.dice) {
+          const diceCount = dice.count || 1;
+          const diceModifier = dice.modifier || 0;
+          const rolls: Array<number> = Array.from(
+              {length: diceCount},
+              () => Math.floor(Math.random() * dice.sides) + 1
+          );
+          const rollsTotal = rolls.reduce((a, b) => a + b, 0) + diceModifier;
+          step.total += rollsTotal;
+          step.rolls.push({
+              count: diceCount,
+              sides: dice.sides,
+              modifier: diceModifier,
+              rolls: rolls,
+              total: rollsTotal
+          });
+      }
+      rollResponse.step.push(step);
+      rollCount--;
+  }
+
+  return rollResponse;
 });
 
 try {
