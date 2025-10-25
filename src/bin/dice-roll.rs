90b5cfe6fb7180fbf932c298703afc321892e30b
Jordan Paris <hamologist.proton.dev>
2025-10-25 15:03:38 -0400

    Implements the CLI interface for project
    - Also includes an expansion to the library logic that simplifies
      rolling with user input. dice_roll::parser::parse allows a user to
      roll using Strings like "1d20 + 1d4 + 2".

--- src/bin/dice-roll.rs
+++ src/bin/dice-roll.rs
@@ -1,22 +1,58 @@
-use dice_roll::{Dice, RollRequest};
-use serde_json;
+use clap::Parser;
+use clap_stdin::FileOrStdin;
+use dice_roll::{parser};
 
-fn main() {
-    match (RollRequest {
-        dice: vec![Dice {
-            count: 2,
-            modifier: 1,
-            sides: 4,
-        }],
-    }
-    .roll_dice())
-    {
-        Ok(result) => match serde_json::to_string_pretty(&result) {
-            Ok(serialized) => println!("{}", serialized),
-            Err(_) => println!("Failed to serialize response."),
-        },
-        Err(err) => {
-            println!("{}", err.to_string())
+
+#[derive(Debug, Parser)]
+struct Args {
+    #[arg(default_value = "-")]
+    input: FileOrStdin,
+
+    #[clap(long, action)]
+    as_json: bool,
+}
+
+fn main() -> Result<(), ()> {
+    let args = Args::parse();
+    let input = match args.input.contents() {
+        Ok(value) => value,
+        Err(_) => {
+            println!("Failed to read input.");
+            return Err(());
         }
     };
+
+
+    let roll_request = match parser::parse(input) {
+        Ok(roll_request) => roll_request,
+        Err(e) => {
+            println!("{}", e.to_string());
+            return Err(());
+        }
+    };
+    let result = match roll_request.roll_dice() {
+        Ok(result) => result,
+        Err(e) => {
+            println!("{}", e.to_string());
+            return Err(());
+        }
+    };
+
+    match args.as_json {
+        true => {
+            match result.to_json() {
+                Ok(serialized) => {
+                    println!("{}", serialized)
+                }
+                Err(e) => {
+                    println!("{}", e.to_string())
+                }
+            }
+        }
+        false => {
+            println!("{}", result.to_string())
+        }
+    }
+
+    return Ok(());
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,31 +1,32 @@
 use rand::prelude::*;
 use serde::{Deserialize, Serialize};
 
-struct BoundConstraint {
-    lower_bound: i32,
-    upper_bound: i32,
-}
 
-struct DiceConstraints {
-    sides: BoundConstraint,
-    modifier: BoundConstraint,
-    count: BoundConstraint,
-}
+mod constraints {
+    pub struct BoundConstraint {
+        pub lower_bound: i32,
+        pub upper_bound: i32,
+    }
 
-const DICE_CONSTRAINTS: DiceConstraints = DiceConstraints {
-    sides: BoundConstraint {
-        lower_bound: 1,
-        upper_bound: 1000,
-    },
-    modifier: BoundConstraint {
-        lower_bound: -100,
-        upper_bound: 100,
-    },
-    count: BoundConstraint {
-        lower_bound: 1,
-        upper_bound: 100,
-    },
-};
+    pub mod dice {
+        use crate::constraints::{BoundConstraint as BC};
+
+        pub const SIDES: BC = BC {
+            lower_bound: 1,
+            upper_bound: 1000,
+        };
+
+        pub const MODIFIER: BC = BC {
+            lower_bound: -100,
+            upper_bound: 100,
+        };
+
+        pub const COUNT: BC = BC {
+            lower_bound: 1,
+            upper_bound: 100,
+        };
+    }
+}
 
 pub enum RollRequestErrors {
     InvalidDiceSides { value: i32 },
@@ -34,26 +35,29 @@ pub enum RollRequestErrors {
 }
 
 impl RollRequestErrors {
+
     pub fn to_string(self) -> String {
+        use crate::constraints::dice::{SIDES, MODIFIER, COUNT};
+
         match self {
             RollRequestErrors::InvalidDiceSides { value } => {
                 format!(
                     "Dice sides must be between {} and {}, {} provided",
-                    DICE_CONSTRAINTS.sides.lower_bound, DICE_CONSTRAINTS.sides.upper_bound, value
+                    SIDES.lower_bound, SIDES.upper_bound, value
                 )
             }
             RollRequestErrors::InvalidDiceModifier { value } => {
                 format!(
                     "Dice modifier must be between {} and {}, {} provided",
-                    DICE_CONSTRAINTS.modifier.lower_bound,
-                    DICE_CONSTRAINTS.modifier.upper_bound,
+                    MODIFIER.lower_bound,
+                    MODIFIER.upper_bound,
                     value
                 )
             }
             RollRequestErrors::InvalidDiceCount { value } => {
                 format!(
                     "Dice count must be between {} and {}, {} provided",
-                    DICE_CONSTRAINTS.count.lower_bound, DICE_CONSTRAINTS.count.upper_bound, value
+                    COUNT.lower_bound, COUNT.upper_bound, value
                 )
             }
         }
@@ -84,25 +88,28 @@ struct Rolls {
 #[derive(Serialize, Debug)]
 pub struct RollResponse {
     rolls: Vec<Rolls>,
+    total: i32,
 }
 
 impl RollRequest {
     fn validate_roll_request(roll_request: RollRequest) -> Result<RollRequest, RollRequestErrors> {
+        use crate::constraints::dice::{SIDES, MODIFIER, COUNT};
+
         for dice in roll_request.dice.iter() {
-            if dice.sides < DICE_CONSTRAINTS.sides.lower_bound
-                || dice.sides > DICE_CONSTRAINTS.sides.upper_bound
+            if dice.sides < SIDES.lower_bound
+                || dice.sides > SIDES.upper_bound
             {
                 return Err(RollRequestErrors::InvalidDiceSides { value: dice.sides });
             }
-            if dice.modifier < DICE_CONSTRAINTS.modifier.lower_bound
-                || dice.modifier > DICE_CONSTRAINTS.modifier.upper_bound
+            if dice.modifier < MODIFIER.lower_bound
+                || dice.modifier > MODIFIER.upper_bound
             {
                 return Err(RollRequestErrors::InvalidDiceModifier {
                     value: dice.modifier,
                 });
             }
-            if dice.count < DICE_CONSTRAINTS.count.lower_bound
-                || dice.count > DICE_CONSTRAINTS.count.upper_bound
+            if dice.count < COUNT.lower_bound
+                || dice.count > COUNT.upper_bound
             {
                 return Err(RollRequestErrors::InvalidDiceCount { value: dice.count });
             }
@@ -118,7 +125,7 @@ impl RollRequest {
         };
 
         let mut rng = rand::rng();
-        let mut roll_response = RollResponse { rolls: Vec::new() };
+        let mut roll_response = RollResponse { rolls: Vec::new(), total: 0 };
 
         for dice in roll_request.dice.iter() {
             let mut rolls = Vec::new();
@@ -136,8 +143,238 @@ impl RollRequest {
                 total: rolls_total,
                 rolls,
             });
+            roll_response.total += rolls_total;
         }
 
         Ok(roll_response)
+    }
+}
+
+impl RollResponse {
+    pub fn to_string(self) -> String {
+        let mut result = Vec::new();
+
+        for rolls in self.rolls {
+            for roll in rolls.rolls {
+                if result.len() > 0 {
+                    result.push("+".to_string())
+                }
+                result.push(format!("({} of {})", roll, rolls.sides));
+            }
+            if rolls.modifier != 0 {
+                if rolls.modifier > 0 {
+                    result.push("+".to_string());
+                    result.push(rolls.modifier.to_string());
+                } else {
+                    result.push("-".to_string());
+                    result.push(rolls.modifier.abs().to_string());
+                }
+            }
+        }
+        result.push("=".to_string());
+        result.push(self.total.to_string());
+
+        return result.join(" ");
+    }
+
+    pub fn to_json(self) -> Result<String, String> {
+        match serde_json::to_string_pretty(&self) {
+            Ok(serialized) => return Ok(serialized),
+            Err(_) => return Err("Failed to serialize response.".to_string()),
+        }
+    }
+}
+
+pub mod parser {
+    #[derive(Debug)]
+    enum Tokens {
+        Roll,
+        PlusRollOrModifier,
+        MinusModifier,
+        Operator,
+    }
+
+    #[derive(Debug)]
+    pub enum ParserErrors {
+        RollParserError(RollTokenParserErrors),
+        EmptyDiceForModifierError,
+        ModifierParserError { token: String },
+    }
+
+    #[derive(Debug)]
+    pub enum RollTokenParserErrors {
+        DiceCountParserError { token: String },
+        DiceSidesParserError { token: String },
+    }
+
+    pub fn parse(input: String) -> Result<crate::RollRequest, ParserErrors> {
+        let input = input.as_bytes();
+        let mut result = crate::RollRequest { dice: Vec::new() };
+        let mut current_token_state = Tokens::Roll;
+        let mut cursor = 0;
+        let mut token = String::new();
+        let input_end = input.len() - 1;
+        while cursor < input_end + 1 {
+            let byte = input[cursor];
+            if byte == b' ' {
+                cursor += 1;
+                continue;
+            }
+
+            match current_token_state {
+                Tokens::PlusRollOrModifier if byte == b'd' => {
+                    current_token_state = Tokens::Roll;
+                    token.push(byte as char);
+                    cursor += 1;
+                    continue;
+                }
+                Tokens::Roll | Tokens::PlusRollOrModifier | Tokens::MinusModifier => {
+                    if cursor == input_end && byte != b'+' && byte != b'-' {
+                        token.push(byte as char);
+                    }
+
+                    if cursor == input_end || byte == b'+' || byte == b'-' {
+                        match current_token_state {
+                            Tokens::Roll => {
+                                let dice = match parse_roll_token(&token) {
+                                    Ok(dice) => dice,
+                                    Err(e) => {
+                                        return Err(ParserErrors::RollParserError(e));
+                                    }
+                                };
+                                result.dice.push(dice);
+                                token = String::new();
+                            }
+                            Tokens::PlusRollOrModifier | Tokens::MinusModifier => {
+                                let modifier: i32 = match token.parse() {
+                                    Ok(val) => val,
+                                    Err(_) => {
+                                        return Err(ParserErrors::ModifierParserError { token });
+                                    }
+                                };
+                                match result.dice.last_mut() {
+                                    Some(dice) => {
+                                        match current_token_state {
+                                            Tokens::PlusRollOrModifier => {
+                                                dice.modifier = modifier;
+                                            },
+                                            Tokens::MinusModifier => {
+                                                dice.modifier = -modifier
+                                            },
+                                            _ => unreachable!()
+                                        }
+                                    }
+                                    None => {
+                                        return Err(ParserErrors::EmptyDiceForModifierError)
+                                    }
+                                }
+                                token = String::new();
+                            }
+                            _ => unreachable!()
+                        }
+                        current_token_state = Tokens::Operator;
+
+                        if cursor == input_end {
+                            cursor += 1;
+                        }
+                    } else {
+                        token.push(byte as char);
+                        cursor += 1;
+                    }
+                    continue;
+                }
+                Tokens::Operator => {
+                    match byte {
+                        b'+' => {
+                            current_token_state = Tokens::PlusRollOrModifier;
+                        }
+                        b'-' => {
+                            current_token_state = Tokens::MinusModifier;
+                        }
+                        _ => unreachable!()
+                    }
+                    cursor += 1;
+                    continue;
+                }
+            }
+        }
+
+        return Ok(result);
+    }
+
+    fn parse_roll_token(token: &String) -> Result<crate::Dice, RollTokenParserErrors> {
+        let _token = token.as_bytes();
+        let mut cursor = 0;
+
+        // Parse the number of dice for the roll
+        let mut fragment = String::new();
+        while cursor < token.len() {
+            let byte = _token[cursor];
+            cursor += 1;
+
+            if byte == b' ' {
+                continue;
+            } else if byte == b'd' {
+                break;
+            }
+
+            fragment.push(byte as char);
+        }
+        let count: i32 = match fragment.parse() {
+            Ok(num) => num,
+            Err(_) => {
+                return Err(RollTokenParserErrors::DiceCountParserError { token: token.clone() });
+            }
+        };
+
+        // Parse the sides of the dice for the roll
+        let mut fragment = String::new();
+        while cursor < _token.len() {
+            let byte = _token[cursor];
+            cursor += 1;
+
+            if byte == b' ' {
+                continue;
+            }
+
+            fragment.push(byte as char);
+        }
+        let sides: i32 = match fragment.parse() {
+            Ok(num) => num,
+            Err(_) => {
+                return Err(RollTokenParserErrors::DiceSidesParserError { token: token.clone() });
+            }
+        };
+
+        let result = crate::Dice {
+            count: count,
+            sides: sides,
+            modifier: 0,
+        };
+
+        return Ok(result);
+    }
+
+    impl ParserErrors {
+        pub fn to_string(self) -> String {
+            match self {
+                Self::RollParserError(roll_token_parser_errors) => {
+                    match roll_token_parser_errors {
+                        RollTokenParserErrors::DiceCountParserError { token } => {
+                            return format!("Invalid roll provided, {token}. Failed to parse dice count.");
+                        },
+                        RollTokenParserErrors::DiceSidesParserError { token } => {
+                            return format!("Invalid roll provided, {token}. Failed to parse dice sides.");
+                        }
+                    }
+                },
+                Self::EmptyDiceForModifierError => {
+                    return "No dice roll provided. Dice roll is in the form \"1d4\"".to_string();
+                },
+                Self::ModifierParserError { token } => {
+                    return format!("Invalid modifier provided, {token}.");
+                }
+            }
+        }
     }
 }
